16c41e38c1fa39d2c45612756db08f2d2a2dd608
Emerson19Reis <emerson.oreis@minervafoods.com>
2023-06-30 09:06:06 -0300

    atualização

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const gorduraInfo = document.getElementById('gordura');
     const carneInfo = document.getElementById('carne');
     const naoDeterminadoInfo = document.getElementById('nao-determinado');
+    const videoPreview = document.getElementById('video-preview');
 
+    // Função para exibir a imagem no preview
+    function displayImage(image) {
+        imagePreview.innerHTML = '';
+        imagePreview.appendChild(image);
+    }
+
+    // Evento de upload de arquivo
     fileUpload.addEventListener('change', (event) => {
         const file = event.target.files[0];
         const reader = new FileReader();
@@ -12,10 +20,61 @@ window.addEventListener('DOMContentLoaded', (event) => {
         reader.onload = function (e) {
             const image = document.createElement('img');
             image.src = e.target.result;
-            imagePreview.innerHTML = '';
-            imagePreview.appendChild(image);
+            displayImage(image);
         }
 
         reader.readAsDataURL(file);
+    });
+
+    // Função para tirar foto diretamente da câmera
+    function captureImageFromCamera() {
+        const constraints = { video: true };
+
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then((stream) => {
+                videoPreview.style.display = 'block';
+                videoPreview.srcObject = stream;
+
+                const canvas = document.createElement('canvas');
+                const context = canvas.getContext('2d');
+
+                const captureBtn = document.getElementById('capture-btn');
+                captureBtn.addEventListener('click', () => {
+                    canvas.width = videoPreview.videoWidth;
+                    canvas.height = videoPreview.videoHeight;
+                    context.drawImage(videoPreview, 0, 0, canvas.width, canvas.height);
+
+                    const image = document.createElement('img');
+                    image.src = canvas.toDataURL('image/png');
+                    displayImage(image);
+
+                    videoPreview.style.display = 'none';
+                    videoPreview.srcObject = null;
+                    stream.getTracks().forEach(track => track.stop());
+                });
+            })
+            .catch((error) => {
+                console.error('Erro ao acessar a câmera: ', error);
+            });
+    }
+
+    // Evento do botão "Processar"
+    const processarBtn = document.getElementById('processar-btn');
+    processarBtn.addEventListener('click', () => {
+        // Chamar função de processamento da imagem aqui
+        console.log('Processando imagem...');
+    });
+
+    // Evento do botão "Salvar"
+    const salvarBtn = document.getElementById('salvar-btn');
+    salvarBtn.addEventListener('click', () => {
+        // Chamar função de salvar as informações no banco de dados aqui
+        console.log('Salvando informações...');
+    });
+
+    // Evento do botão "Tirar foto"
+    const captureBtn = document.getElementById('capture-btn');
+    captureBtn.addEventListener('click', () => {
+        captureImageFromCamera();
     });
 });
